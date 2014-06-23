b49a12cda578294f1e6197159d70694a00d5feb4
Mathieu Simon <mathieu.simon@simweb.ch>
2014-06-24 00:09:16 +0200

    Complete user-facing german translation of Pydio
    Sponsored-by: Gymnasium Köniz-Lerbermatt

--- core/src/plugins/access.ajxp_user/i18n/de.php
+++ core/src/plugins/access.ajxp_user/i18n/de.php
@@ -35,7 +35,7 @@ $mess=array(
 "14"=> "Integrität",
 "15"=> "OK",
 "16"=> "Fehlerhaft",
-"17"=> "Download URL",
+"17"=> "Download-URL",
 "18"=> "URL kopieren",
 "19"=> "URL zum Senden per E-Mail kopieren.",
 "20"=> "Downloads",
@@ -44,7 +44,7 @@ $mess=array(
 "23"=> "%s Dateien erfolgreich gelöscht.",
 "24"=> "Nichts zum Entfernen.",
 "25"=> "Entfernen abgelaufen",
-"26"=> "Ausgelaufene freigegebene Dateien entfernen.",
+"26"=> "Abgelaufene freigegebene Dateien entfernen.",
 "27"=> "Besitzer",
 "28"=> "Dateien zum direkten Download mit oder ohne Passwortschutz freigegeben.",
 "29"=> "Repositories von Benutzern übertragen.",
@@ -52,44 +52,44 @@ $mess=array(
 "31"=> "Beschreibung",
 "32"=> "Zurücksetzen",
 "33"=> "Download-Zähler zurücksetzen",
-"title" => "My Dashboard",
+"title" => "Mein Dashboard",
 "desc" => "Manage my account preferences, my address book, shared elements, etc.",
 "34" => "My Shares",
 "35" => "All my shared items (files and folders)",
-"36" => "Parameters",
+"36" => "Einstellungen",
 "37" => "My profile and application settings",
 "38"=> "Notifications",
 "39"=> "Select a workspace",
-"40"=> "Welcome, %s",
+"40"=> "Willkommen, %s",
 "41"=> "Use this workspace as default",
 "42"=> "Enter",
-"43"=> "My Profile",
-"43t"=> "All my personal data",
-"44"=> "My Account",
-"45"=> "Personal data",
-"46"=> "Address Book",
+"43"=> "Mein Profil",
+"43t"=> "All meine persönlichen Daten",
+"44"=> "Mein Konto",
+"45"=> "Persönliche Daten",
+"46"=> "Adressbuch",
 "47"=> "Users and teams that you created",
-"48"=> "My Users",
+"48"=> "Meine Benutzer",
 "49"=> "External users that you created",
-"50"=> "My Teams",
+"50"=> "Meine Teams",
 "51"=> "Teams of users, used as shortcuts when sharing",
 "52"=> "Are you sure you want to delete this team? This will not remove any users.",
-    "53"=> "Getting Started",
+    "53"=> "Erste Schritte",
     "54"=> "Get started with Pydio by following How-to videos",
-    "55"=> "Do you need help <a>to get started?</a>",
+    "55"=> "Brauchen Sie Hilfe für <a>erste Schritte?</a>",
     "56"=> "Getting started with Pydio? Here are some videos for you!",
-    "57"=> "Download Pydio for...",
-    "58"=> "Android device",
+    "57"=> "Pydio herunterladen für...",
+    "58"=> "Android-Gerät",
     "59"=> "iPhone/iPad",
     "60"=>"Mac OS (Beta)",
     "61"=>"Windows (Beta)",
-    "62"=>"<h2>Getting started</h2>
-            This video is an introduction to Pydio web application:
+    "62"=>"<h2>Erste Schritte</h2>
+            Dieses Video zeigt eine Einführunf für die Pydio-Webapplikation:
             <ul>
                 <li>User Dashboard</li>
                 <li>GUI Overview</li>
                 <li>Managing files and folders</li>
-                <li>Previewing and editing files</li>
+                <li>Dateivorschau und -bearbeitung</li>
                 <li>Quick access through bookmarks and search engine</li>
                 <li>Selection cart</li>
             </ul>",
@@ -106,8 +106,8 @@ $mess=array(
     "64"=>"<h2>Sharing folders with Pydio</h2>
                             This video covers the various options to share folders in Pydio
                             <ul>
-                                <li>Share as new workspace</li>
-                                <li>Share as a public minisite</li>
+                                <li>Als neuen workspace teilen</li>
+                                <li>Als öffentliche mini-Seite teilen</li>
             </ul>",
     "65"=> "Discover more on the Pydio channel!",
     "66"=>"<h2>Using Pydio on iOS device</h2>
@@ -115,6 +115,6 @@ $mess=array(
 <ul><li>Setup the server connection</li>
 <li>Navigating through the files</li>
 <li>Keeping files offline and interacting with external applications</li>
-<li>Protecting the app with a PIN-code</li>
-<li>Using bookmarks and search engine</li></ul>",
+<li>App durch einen PIN-Code schützen</li>
+<li>Lesezeichen und Suche verwenden</li></ul>",
 );
