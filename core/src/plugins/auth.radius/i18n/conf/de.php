8f6e78c8f35a43cc0cee818ff0e370f941a4ec14
Mathieu Simon <mathieu.simon@simweb.ch>
2014-06-25 21:43:45 +0200

    i18n: RADIUS authentication, add german, fix english
    Add german translation for the RADIUS module and fix some
    minor nits in the english reference.

---
+++ core/src/plugins/auth.radius/i18n/conf/de.php
@@ -0,0 +1,19 @@
+<?php
+$mess=array(
+"RADIUS Authentification" => "RADIUS-Authentifizierung",
+"Authenticate user by directly connecting to an RADIUS server" => "Benutzer über einen RADIUS-Server authentifizieren",
+"Server" => "Server",
+"IP or DNS name of the RADIUS server" => "IP oder DNS-Name des RADIUS-Servers",
+"Port" => "Port",
+"Port of the RADIUS server" => "Port des RADIUS-Servers",
+"Shared secret" => "Shared Secret",
+"Shared secret of the RADIUS server" => "Gemeinsames Geheimnis (Shared secret) des RADIUS-Servers",
+"Auth type" => "Authentifizierungstyp",
+"Auth type to use for RADIUS server" => "Authentifizierungstyp des RADIUS-Servers",
+"Test user" => "Testbenutzer",
+"User for connection testing" => "Benutzer um die Verbindung zu testen",
+"Test password" => "Testpasswort",
+"Password for connection testing" => "Passwort um die Verbindung zu testen",
+"Test Connection" => "Verbindung testen",
+"Try to connect to RADIUS server" => "Verbindung zum RADIUS-Server testen."
+);
--- core/src/plugins/auth.radius/i18n/conf/en.php
+++ core/src/plugins/auth.radius/i18n/conf/en.php
@@ -1,7 +1,7 @@
 <?php
 $mess=array(
 "RADIUS Authentification" => "RADIUS Authentification",
-"Authenticate user by directly connecting to an RADIUS server" => "Authenticate user by directly connecting to an RADIUS server",
+"Authenticate user by directly connecting to an RADIUS server" => "Authenticate users by directly connecting to a RADIUS server",
 "Server" => "Server",
 "IP or DNS name of the RADIUS server" => "IP or DNS name of the RADIUS server",
 "Port" => "Port",
@@ -9,11 +9,11 @@ $mess=array(
 "Shared secret" => "Shared Secret",
 "Shared secret of the RADIUS server" => "Shared secret of the RADIUS server",
 "Auth type" => "Auth type",
-"Auth type to use for RADIUS server" => "Auth type to use for RADIUS server",
+"Auth type to use for RADIUS server" => "Authentication type to use with the RADIUS server",
 "Test user" => "Test user",
 "User for connection testing" => "User for connection testing",
 "Test password" => "Test password",
 "Password for connection testing" => "Password for connection testing",
-"Test Connection" => "Test Connection",
-"Try to connect to RADIUS server" => "Try to connect to RADIUS server"
+"Test Connection" => "Test connection",
+"Try to connect to RADIUS server" => "Try to connect to the RADIUS server"
 );
