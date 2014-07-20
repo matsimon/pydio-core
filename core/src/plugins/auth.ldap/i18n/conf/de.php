8c42188aaf7ad65706eef05a6af3629550b75bfb
Mathieu Simon <mathieu.simon@simweb.ch>
2014-07-20 11:12:54 +0200

    Temp add german i18n

---
+++ core/src/plugins/auth.ldap/i18n/conf/de.php
@@ -0,0 +1,58 @@
+<?php
+/*
+* Copyright 2007-2013 Charles du Jeu - Abstrium SAS <team (at) pyd.io>
+* This file is part of Pydio.
+*
+* Pydio is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* Pydio is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with Pydio.  If not, see <http://www.gnu.org/licenses/>.
+*
+* The latest code can be found at <http://pyd.io/>.
+*/
+$mess=array(
+"LDAP Directory" => "LDAP-Verzeichnis",
+"Authentication datas are stored on the LDAP server." => "Authentifizierungsdaten sind in einem LDAP-Verzeichnis abgelegt.",
+"LDAP URL" => "LDAP-Servername",
+"LDAP Server URL (IP or name)" => "LDAP Serveradresse (IP oder Hostname)",
+"LDAP Port" => "LDAP Port",
+"LDAP Server Port (leave blank for default)" => "LDAP Serverport (leer lassen für Standardwert 389)",
+"LDAP bind username" => "Anmeldename Bind-Benutzer",
+"Username (uid + dn) of LDAP bind user" => "Benutzername des sog. 'Bind-User' (uid + dn) des LDAP Anmeldebenutzers, bei AD ist auch der UserPrincipalName (username@domain) möglich",
+"LDAP bind password" => "LDAP bind Password",
+"Password of LDAP bind user" => "Passwort des LDAP bind-Benutzer",
+"People DN" => "Benutzerkontext (DN)",
+"DN where the users are stored" => "Kontext im LDAP-Verzichnis in dem Benutzer abgelegt sind",
+"LDAP Filter" => "LDAP-Filter",
+"Filter which users to fetch." => "Filter, der nur bestimmte User / Objektetypen ausliest.",
+"User attribute" => "Attribut Benutzerrname",
+"Username attribute" => "Attribut Benutzername",
+"LDAP/AD Directory" => "LDAP/AD-Verzeichnis",
+"Authentication datas are stored in an LDAP/AD directory." => "Authentifizierungsdaten befinden sich in einem LDAP-/AD-Verzeichnis.",
+"Protocol" => "Protokoll",
+"Connect through ldap or ldaps" => "Verbindung zum Verzeichnis über LDAP der LDAPS",
+"Groups DN" => "Gruppenkontext (DN)",
+"DN where the groups are stored. Must be used in cunjonction with a group parameter mapping, generally using the memberOf feature." => "DN where the groups are stored. Must be used in cunjonction with a group parameter mapping, generally using the memberOf feature.",
+"LDAP Groups Filter" => "LDAP-Gruppenfilter",
+"Filter which groups to fetch." => "Filter which groups to fetch.",
+"Group attribute" => "Attribute GruppenGruppenattribut",
+"Group main attribute to be used as a label" => "Group main attribute to be used as a label",
+"LDAP attribute" => "LDAP-Attribut",
+"Name of the LDAP attribute to read" => "Bezeichnung des LDAP-Attributes, welches ausgelesen werden soll.d",
+"Mapping Type" => "Zuordnungstype",
+"Determine the type of mapping" => "Determine the type of mapping",
+"Plugin parameter" => "Plugin-Parameter",
+"Name of the custom local parameter to set" => "Name of the custom local parameter to set",
+"Test User" => "Testbenutzer",
+"Use the Test connection button to check if this user is correctly found in your LDAP directory." => "Use the Test connection button to check if this user is correctly found in your LDAP directory.",
+"Test connection" => "Verbindung testen",
+"Try to connect to LDAP" => "Verbindung zum LDAP-Verzeichnis mit konfigurierten Parametern testen",
+);
